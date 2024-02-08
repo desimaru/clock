bfe5c1c7be444d9784a6256ee0a77f28388040f9
デシマル <157348086+desimaru@users.noreply.github.com>
2024-02-08 11:03:02 +0900

    機能追加

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
-document.querySelector("#local").checked = true;
+const body = document.querySelector("body"),
+    h1 = document.querySelector("h1"),
+    local = document.getElementById("local"),
+    utc = document.getElementById("utc"),
+    year = document.getElementById("year"),
+    ms = document.getElementById("ms"),
+    color = document.getElementById("color"),
+    color2 = document.getElementById("color2"),
+    number = document.getElementById("number");
+let time2 = "",
+    a = "";
+local.checked = true;
+color2.value = "#FFFFFF";
+number.value = 80;
 setInterval(() => {
-    const time = new Date(),
-        h1 = document.querySelector("h1");
-    let time2 = "",
-        a = "";
-    if (document.querySelector("#local").checked) {
-        if (document.querySelector("#year").checked) {
+    const time = new Date();
+    time2 = "";
+    if (local.checked) {
+        if (year.checked) {
             a = "";
             switch (time.getDay()) {
                 case 0:
@@ -28,22 +39,17 @@ setInterval(() => {
                     break;
                 case 6:
                     a = "土曜日";
-            }
-            time2 += time.toLocaleDateString() + a;
-            if (!document.querySelector("#ms")) {
-                h1.style.fontSize = "100px";
-            } else {
-                h1.style.fontSize = "80px";
-            }
-        } else h1.style.fontSize = "150px";
+            };
+            time2 += `${time.getFullYear()
+                }/${`0${time.getMonth() + 1}`.slice(-2)}/${`0${time.getDate()}`.slice(-2)} ${a} `;
+        };
         time2 += time.toLocaleTimeString("ja");
-        if (document.querySelector("#ms").checked) {
+        if (ms.checked) {
             time2 += `.${`00${time.getMilliseconds()}`.slice(-3)}`;
         }
-        h1.innerHTML = time2;
-    } else if (document.querySelector("#utc").checked) {
+    } else if (utc.checked) {
         a = "";
-        switch (time.getDay()) {
+        switch (time.getUTCDay()) {
             case 0:
                 a = "日曜日";
                 break;
@@ -65,17 +71,19 @@ setInterval(() => {
             case 6:
                 a = "土曜日";
         }
-        if (document.querySelector("#year").checked) {
-            time2 += `${time.getUTCFullYear()}/${`0${
-                time.getUTCMonth() + 1
-            }`.slice(-2)}/${`0${time.getUTCDate()}`.slice(-2)} ${a} `;
-        }
+        if (year.checked) {
+            time2 += `${time.getUTCFullYear()}/${`0${time.getUTCMonth() + 1
+                }`.slice(-2)}/${`0${time.getUTCDate()}`.slice(-2)} ${a} `;
+        };
         time2 += `${`0${time.getUTCHours()}`.slice(
             -2
         )}:${time.getUTCMinutes()}:${time.getUTCSeconds()}`;
-        if (document.querySelector("#ms").checked) {
+        if (ms.checked) {
             time2 += `.${`00${time.getUTCMilliseconds()}`.slice(-3)}`;
         }
-        h1.innerHTML = time2;
     }
+    h1.innerHTML = time2;
+    body.style.color = color.value;
+    body.style.backgroundColor = color2.value;
+    h1.style.fontSize = `${number.value}px`;
 });
