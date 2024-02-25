fe3fe02afcc49b4878e7839e31be58256e82898a
デシマル <157348086+desimaru@users.noreply.github.com>
2024-02-25 16:12:55 +0900

    Improve: 曜日判別を関数化

--- index.js
+++ index.js
@@ -2,27 +2,45 @@
 const body = document.querySelector("body"),
     /**@type {HTMLHeadingElement} */
     h1 = document.querySelector("h1"),
-    /**@type {HTMLFormElement} */
-    local = document.getElementById("local"),
-    utc = document.getElementById("utc"),
-    year = document.getElementById("year"),
-    ms = document.getElementById("ms"),
-    color = document.getElementById("color"),
-    color2 = document.getElementById("color2"),
-    number = document.getElementById("number");
+    /**@type {HTMLInputElement[]} */
+    [utc, local, year, ms, color, color2, number] =
+        document.getElementsByTagName("input");
 /**
  * 表示する時間
- * @type {String}
-*/
+ * @type {string}
+ */
 let time2 = "",
     /**
      * 曜日
-     * @type {String}
+     * @type {string}
      */
     a = "";
 local.checked = true;
 color2.value = "#FFFFFF";
 number.value = 80;
+/**
+ * 曜日判別
+ * @param {1|2|3|4|5|6} date
+ * @returns {"日"|"月"|"火"|"水"|"木"|"金"|"土"} 曜日
+ */
+function day(date) {
+    switch (date) {
+        case 0:
+            return "日";
+        case 1:
+            return "月";
+        case 2:
+            return "火";
+        case 3:
+            return "水";
+        case 4:
+            return "木";
+        case 5:
+            return "金";
+        case 6:
+            return "土";
+    }
+}
 setInterval(() => {
     /**
      * 現在の時刻
@@ -33,68 +51,31 @@ setInterval(() => {
     if (local.checked) {
         // 現地時間にチェックが入ってる時
         if (year.checked) {
-            a = "";
-            switch (time.getDay()) {
-                case 0:
-                    a = "日曜日";
-                    break;
-                case 1:
-                    a = "月曜日";
-                    break;
-                case 2:
-                    a = "火曜日";
-                    break;
-                case 3:
-                    a = "水曜日";
-                    break;
-                case 4:
-                    a = "木曜日";
-                    break;
-                case 5:
-                    a = "金曜日";
-                    break;
-                case 6:
-                    a = "土曜日";
-            };
-            time2 += `${time.getFullYear()
-                }/${`0${time.getMonth() + 1}`.slice(-2)}/${`0${time.getDate()}`.slice(-2)} ${a} `;
-        };
-        time2 += time.toLocaleTimeString("ja");
+            // `(年)/(月)/(日) (曜日)
+            time2 += `${time.getFullYear()}/${`0${time.getMonth() + 1}`.slice(
+                -2
+            )}/${`0${time.getDate()}`.slice(-2)} ${day(time.getDay())}曜日 `;
+        }
+        time2 += time.toLocaleTimeString();
         if (ms.checked) {
             time2 += `.${`00${time.getMilliseconds()}`.slice(-3)}`;
         }
     } else if (utc.checked) {
         // 世界標準時にチェックが入ってる時
-        a = "";
-        switch (time.getUTCDay()) {
-            case 0:
-                a = "日曜日";
-                break;
-            case 1:
-                a = "月曜日";
-                break;
-            case 2:
-                a = "火曜日";
-                break;
-            case 3:
-                a = "水曜日";
-                break;
-            case 4:
-                a = "木曜日";
-                break;
-            case 5:
-                a = "金曜日";
-                break;
-            case 6:
-                a = "土曜日";
-        }
         if (year.checked) {
-            time2 += `${time.getUTCFullYear()}/${`0${time.getUTCMonth() + 1
-                }`.slice(-2)}/${`0${time.getUTCDate()}`.slice(-2)} ${a} `;
-        };
+            // (年)/(月)/(日) (曜日)
+            time2 += `${time.getUTCFullYear()}/${`0${
+                time.getUTCMonth() + 1
+            }`.slice(-2)}/${`0${time.getUTCDate()}`.slice(-2)} ${day(
+                time.getUTCDay()
+            )}曜日 `;
+        }
+        // (時)/(分)/(秒)
         time2 += `${`0${time.getUTCHours()}`.slice(
             -2
-        )}:${time.getUTCMinutes()}:${time.getUTCSeconds()}`;
+        )}:${`0${time.getUTCMinutes()}`.slice(
+            -2
+        )}:${`0${time.getUTCSeconds()}`.slice(-2)}`;
         if (ms.checked) {
             time2 += `.${`00${time.getUTCMilliseconds()}`.slice(-3)}`;
         }
