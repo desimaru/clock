1f47c366b9c923753538b447baf2b7eab70b69bb
デシマル <157348086+desimaru@users.noreply.github.com>
2024-06-14 23:50:13 +0900

    feat: パフォーマンスの改善のため、コードの改善や設定等を追加。
    - 何ミリ秒毎に更新するかの欄
    - 24時間表記またはPM/AMで表記するかを切り替えるチェックボックス

--- index.js
+++ index.js
@@ -1,66 +1,93 @@
-{
 /**@type {HTMLBodyElement} */
 const body = document.querySelector("body"),
     /**@type {HTMLHeadingElement} */
     h1 = document.querySelector("h1"),
     /**@type {HTMLInputElement[]} */
-    [utc, local, year, ms, color, color2, number] =
+    [, local, year, ms, format, color, color2, number, rate] =
         document.getElementsByTagName("input");
-/**
- * 表示する時間
- * @type {string}
- */
-let time2 = "",
-    /**
-     * 曜日
-     * @type {string}
-     */
-    a = "";
 local.checked = true;
 color2.value = "#FFFFFF";
 number.value = 80;
-setInterval(() => {
-    /**
-     * 現在の時刻
-     * @type {Date}
-     */
-    const time = new Date();
-    time2 = "";
-    if (local.checked) {
-        // 現地時間にチェックが入ってる時
-        if (year.checked) {
-            // `(年)/(月)/(日) (曜日)
-            time2 += `${time.getFullYear()}/${`0${time.getMonth() + 1}`.slice(
-                -2
-            )}/${`0${time.getDate()}`.slice(-2)} ${"日月火水木金土"[time.getDay()]}曜日 `;
+rate.value = 200;
+(async function () {
+    for (;;) {
+        /**
+         * 現在の時刻
+         * @type {Date}
+         */
+        const DATE_TIME = new Date();
+        /**
+         * 表示する時間
+         * @type {string}
+         */
+        let time = "";
+        if (local.checked) {
+            // 現地時間にチェックが入ってる時
+            if (year.checked) {
+                // `(年)/(月)/(日) (曜日)
+                time = `${[
+                    DATE_TIME.getFullYear(),
+                    `0${DATE_TIME.getMonth() + 1}`.slice(-2),
+                    `0${DATE_TIME.getDate()}`.slice(-2),
+                ].join("/")} ${`${"日月火水木金土"[DATE_TIME.getDay()]}曜日`}`;
+            }
+            if (format.checked) {
+                time += `0${DATE_TIME.toLocaleTimeString("ja")}`.slice(-8);
+            } else {
+                time += `${[
+                    `0${DATE_TIME.getHours() % 12}`.slice(-2),
+                    `0${DATE_TIME.getMinutes()}`.slice(-2),
+                    `0${DATE_TIME.getSeconds()}`.slice(-2),
+                ].join(":")}`;
+            }
+            if (ms.checked) {
+                time += `.${`00${DATE_TIME.getMilliseconds()}`.slice(-3)}`;
+            }
+            if (!format.checked) {
+                time += DATE_TIME.toLocaleTimeString("en").slice(-3);
+            }
+        } else {
+            // 世界標準時にチェックが入ってる時
+            if (year.checked) {
+                // (年)/(月)/(日) (曜日)
+                time += `${[
+                    DATE_TIME.getUTCFullYear(),
+                    `0${DATE_TIME.getUTCMonth() + 1}`.slice(-2),
+                    `0${DATE_TIME.getUTCDate()}`.slice(-2),
+                ].join("/")} ${"日月火水木金土"[DATE_TIME.getUTCDay()]}曜日 `;
+            }
+            if (format.checked) {
+                // (時)/(分)/(秒)
+                time += [
+                    `0${DATE_TIME.getUTCHours()}`.slice(-2),
+                    `0${DATE_TIME.getUTCMinutes()}`.slice(-2),
+                    `0${DATE_TIME.getUTCSeconds()}`.slice(-2),
+                ].join(":");
+            } else {
+                time += [
+                    `0${DATE_TIME.getUTCHours() % 12}`.slice(-2),
+                    `0${DATE_TIME.getUTCMinutes()}`.slice(-2),
+                    `0${DATE_TIME.getSeconds()}`.slice(-2),
+                ].join(":");
+            }
+            if (ms.checked) {
+                time += `.${`00${DATE_TIME.getUTCMilliseconds()}`.slice(-3)}`;
+            }
+            if (!format.checked) {
+                time += DATE_TIME.toLocaleTimeString("en").slice(-3);
+            }
         }
-        time2 += time.toLocaleTimeString("ja");
-        if (ms.checked) {
-            time2 += `.${`00${time.getMilliseconds()}`.slice(-3)}`;
-        }
-    } else if (utc.checked) {
-        // 世界標準時にチェックが入ってる時
-        if (year.checked) {
-           // (年)/(月)/(日) (曜日)
-            time2 += `${time.getUTCFullYear()}/${`0${
-                time.getUTCMonth() + 1
-            }`.slice(-2)}/${`0${time.getUTCDate()}`.slice(-2)} ${"日月火水木金土"[time.getUTCDay()]}曜日 `;
-        }
-        // (時)/(分)/(秒)
-        time2 += `${`0${time.getUTCHours()}`.slice(
-            -2
-        )}:${`0${time.getUTCMinutes()}`.slice(
-            -2
-        )}:${`0${time.getUTCSeconds()}`.slice(-2)}`;
-        if (ms.checked) {
-            time2 += `.${`00${time.getUTCMilliseconds()}`.slice(-3)}`;
-        }
+        // h1に反映する
+        h1.innerHTML = time;
+        h1.style.fontSize = `${number.value}px`;
+        // bodyのスタイルを変える
+        body.style.color = color.value;
+        body.style.backgroundColor = color2.value;
+        await new Promise((resolve) =>
+            setTimeout(
+                () => resolve(),
+                !Number.isNaN(Number(rate.value)) ? Number(rate.value) : 100
+            )
+        );
     }
-    // h1に反映する
-    h1.innerHTML = time2;
-    h1.style.fontSize = `${number.value}px`;
-    // bodyのスタイルを変える
-    body.style.color = color.value;
-    body.style.backgroundColor = color2.value;
-});
-}
+})();
