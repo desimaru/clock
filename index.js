83b4df717418ffdb451c703e642da98ebd648419
desimaru <157348086+desimaru@users.noreply.github.com>
2025-04-09 17:32:55 +0900

    refactor: 可読性のため変数名を変更

--- index.js
+++ index.js
@@ -1,73 +1,66 @@
 const body = document.body,
     h1 = document.querySelector("h1"),
-    [, local, year, ms, format, color, color2, number] =
+    [, isLocalTime, isShowYear, isShowMS, is24hour, color, backgroundColor, fontSize] =
         document.getElementsByTagName("input");
-local.checked = true;
-color2.value = "#FFFFFF";
-number.value = 80;
 function reflesh() {
-    const DATE_TIME = new Date();
+    const dateTime = new Date();
     let time = "";
-    if (local.checked) {
+    if (isLocalTime.checked) {
         // 現地時間にチェックが入ってる時
-        if (year.checked) {
+        if (isShowYear.checked) {
             // `(年)/(月)/(日) (曜日)
             time = `${[
-                DATE_TIME.getFullYear(),
-                `0${DATE_TIME.getMonth() + 1}`.slice(-2),
-                `0${DATE_TIME.getDate()}`.slice(-2),
-            ].join("/")} ${`${"日月火水木金土"[DATE_TIME.getDay()]}曜日`}`;
+                dateTime.getFullYear(),
+                `0${dateTime.getMonth() + 1}`.slice(-2),
+                `0${dateTime.getDate()}`.slice(-2),
+            ].join("/")} ${`${"日月火水木金土"[dateTime.getDay()]}曜日`}`;
         }
-        if (format.checked) {
-            time += `0${DATE_TIME.toLocaleTimeString("ja")}`.slice(-8);
+        if (is24hour.checked) {
+            time += `0${dateTime.toLocaleTimeString("ja")}`.slice(-8);
         } else {
             time += [
-                `0${DATE_TIME.getHours() % 12}`.slice(-2),
-                `0${DATE_TIME.getMinutes()}`.slice(-2),
-                `0${DATE_TIME.getSeconds()}`.slice(-2),
+                `0${dateTime.getHours() % 12}`.slice(-2),
+                `0${dateTime.getMinutes()}`.slice(-2),
+                `0${dateTime.getSeconds()}`.slice(-2),
             ].join(":");
         }
-        if (ms.checked) {
-            time += `.${`00${DATE_TIME.getMilliseconds()}`.slice(-3)}`;
-        }
-        if (!format.checked) {
-            time += DATE_TIME.toLocaleTimeString("en").slice(-3);
-        }
+        if (isShowMS.checked)
+            time += `.${`00${dateTime.getMilliseconds()}`.slice(-3)}`;
+        if (!is24hour.checked)
+            time += dateTime.toLocaleTimeString("en").slice(-3);
     } else {
         // 世界標準時にチェックが入ってる時
-        if (year.checked) {
+        if (isShowYear.checked) {
             // (年)/(月)/(日) (曜日)
             time += `${[
-                DATE_TIME.getUTCFullYear(),
-                `0${DATE_TIME.getUTCMonth() + 1}`.slice(-2),
-                `0${DATE_TIME.getUTCDate()}`.slice(-2),
-            ].join("/")} ${"日月火水木金土"[DATE_TIME.getUTCDay()]}曜日 `;
+                dateTime.getUTCFullYear(),
+                `0${dateTime.getUTCMonth() + 1}`.slice(-2),
+                `0${dateTime.getUTCDate()}`.slice(-2),
+            ].join("/")} ${"日月火水木金土"[dateTime.getUTCDay()]}曜日 `;
         }
-        if (format.checked) {
+        if (is24hour.checked) {
             // (時)/(分)/(秒)
             time += [
-                `0${DATE_TIME.getUTCHours()}`.slice(-2),
-                `0${DATE_TIME.getUTCMinutes()}`.slice(-2),
-                `0${DATE_TIME.getUTCSeconds()}`.slice(-2),
+                `0${dateTime.getUTCHours()}`.slice(-2),
+                `0${dateTime.getUTCMinutes()}`.slice(-2),
+                `0${dateTime.getUTCSeconds()}`.slice(-2),
             ].join(":");
         } else {
             time += [
-                `0${DATE_TIME.getUTCHours() % 12}`.slice(-2),
-                `0${DATE_TIME.getUTCMinutes()}`.slice(-2),
-                `0${DATE_TIME.getSeconds()}`.slice(-2),
+                `0${dateTime.getUTCHours() % 12}`.slice(-2),
+                `0${dateTime.getUTCMinutes()}`.slice(-2),
+                `0${dateTime.getSeconds()}`.slice(-2),
             ].join(":");
         }
-        if (ms.checked) {
-            time += `.${`00${DATE_TIME.getUTCMilliseconds()}`.slice(-3)}`;
-        }
-        if (!format.checked) {
-            time += DATE_TIME.toLocaleTimeString("en").slice(-3);
-        }
+        if (isShowMS.checked)
+            time += `.${`00${dateTime.getUTCMilliseconds()}`.slice(-3)}`;
+        if (!is24hour.checked)
+            time += dateTime.toLocaleTimeString("en").slice(-3);
     }
-    h1.innerHTML = time;
-    h1.style.fontSize = `${number.value}px`;
+    h1.textContent = time;
+    h1.style.fontSize = `${fontSize.value}px`;
     body.style.color = color.value;
-    body.style.backgroundColor = color2.value;
+    body.style.backgroundColor = backgroundColor.value;
     requestAnimationFrame(reflesh);
 }
 requestAnimationFrame(reflesh);
